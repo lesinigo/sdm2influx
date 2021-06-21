67c054216426aab3dc55585d96b831c19c095bf6
Luca Lesinigo <luca@lesinigo.it>
2021-06-21 15:50:19 +0200

    bump minor release

--- sdm2influx.py
+++ sdm2influx.py
@@ -15,7 +15,7 @@ from influxdb.exceptions import InfluxDBServerError
 import pymodbus.client.sync
 from retrying import retry
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 logger = logging.getLogger(__name__)
 
